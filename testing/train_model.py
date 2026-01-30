2bc97b7b0b44b47e346eace8debc170a274fc7ad
Ryan Yeung <ryeung0305@gmail.com>
2026-01-30 10:26:37 -0800

    more simplifying

--- testing/train_model.py
+++ testing/train_model.py
@@ -13,7 +13,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from api import api_function
 from data import cuisine_data, data_functions, user_data, restaurant_data, interact_data
-from ML import reccomendation
+from ML import reccomendation, ml_model
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,44 +21,7 @@ load_dotenv()
 
 connection = data_functions.get_connection("test_data.db")
 
-
-#Gathers needed data
-feature_data = data_functions.join_interaction_restaurant(connection)
-frequency = api_function.find_frequency(connection)
-
-#Separates feature data from user response
-features, response = api_function.insert_frequency(feature_data, frequency)
-
-#CREATE TRAINING SPLIT
-x_train, x_test, y_train, y_test = train_test_split(features, response, test_size=0.2, random_state=42)
-
-#Scaling feature data so all features are considered equally
-logistic_scaler = StandardScaler()
-x_train_scaled = logistic_scaler.fit_transform(x_train)
-x_test_scaled = logistic_scaler.transform(x_test)
-
-logistic_model = LogisticRegression(max_iter=1000, C=1.0)        #LogisticRegression()
-
-logistic_model.fit(x_train_scaled, y_train)
-
-prediction = logistic_model.predict(x_test_scaled)
-
-score = logistic_model.score(x_test_scaled, y_test)
-
-print(f"Accuracy of % {score*100}")
-
-#CROSS VALIDATION
-scaler2 = StandardScaler()
-all_x_scaled = scaler2.fit_transform(features)
-cv_scores = cross_val_score(logistic_model, all_x_scaled, response, cv=5)
-print(f"Accuracy scores of other tests: {cv_scores}")
-
-
-#CHECKING FOR OVER FITTING
-train_score = logistic_model.score(x_train_scaled, y_train)
-test_score = logistic_model.score(x_test_scaled, y_test)
-print(f"Train: {train_score:.1%}, Test: {test_score:.1%}, Diff: {abs(train_score-test_score):.1%}")
-
+logistic_model, logistic_scaler, score = ml_model.train_save_model(connection)
 
 
 #LOGISTIC REGRESSION FEATURE IMPORTANCE
@@ -126,24 +89,6 @@ for restaurant in top10:            #(11) show user restaurant
             print("save accept")          #Use userid in session & attributes["place_id"] to find cuisine and update acceptance
       else:
             print("save rejection")'''
-
-#Saving model and scaler to ML file
-
-models_dir = Path(__file__).parent.parent / 'ml' / 'models'
-models_dir.mkdir(parents=True, exist_ok=True)
-
-# Save model
-model_path = models_dir / 'model.pkl'
-pickle.dump(logistic_model, open(model_path, 'wb'))
-print(f"Model saved to {model_path}")
-
-# Save scaler
-scaler_path = models_dir / 'scaler.pkl'
-pickle.dump(logistic_scaler, open(scaler_path, 'wb'))
-print(f"Scaler saved to {scaler_path}")
-
-
-      
 
 
 connection.close()
