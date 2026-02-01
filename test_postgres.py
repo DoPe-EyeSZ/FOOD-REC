3e93d14ca1fdc8dadb16eb4e9e7192bb77411a06
Ryan Yeung <ryeung0305@gmail.com>
2026-01-31 23:20:55 -0800

    deleted old tables; created new ones

--- test_postgres.py
+++ test_postgres.py
@@ -3,8 +3,6 @@ from data import data_functions, cuisine_data, interact_data, restaurant_data, u
 try:
     connection = data_functions.get_connection("test")
 
-    
-    
     user_data.create_user_table(connection)
     restaurant_data.create_restaurant_table(connection)
     cuisine_data.create_cuisine_table(connection)
