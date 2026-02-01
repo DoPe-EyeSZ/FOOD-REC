dc852013809d5065b2733040bdb5873476b4196f
Ryan Yeung <ryeung0305@gmail.com>
2026-01-31 17:05:51 -0800

    further testing to make sure it works

--- test_postgres.py
+++ test_postgres.py
@@ -2,10 +2,8 @@ from data import data_functions
 
 try:
     connection = data_functions.get_connection(db_type="prod")
-    print("data success")
 
     connection2 = data_functions.get_connection("test")
-    print("test success")
     connection.close()
 
 except Exception as e:
