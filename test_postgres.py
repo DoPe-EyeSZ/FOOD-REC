8d2bc2d2785e187b5d2e71075133e31003faf499
Ryan Yeung <ryeung0305@gmail.com>
2026-01-31 17:04:42 -0800

    testing if established connectgion works

--- test_postgres.py
+++ test_postgres.py
@@ -1,8 +1,11 @@
 from data import data_functions
 
 try:
-    connection = data_functions.get_connection(db_type="production")
+    connection = data_functions.get_connection(db_type="prod")
     print("data success")
+
+    connection2 = data_functions.get_connection("test")
+    print("test success")
     connection.close()
 
 except Exception as e:
